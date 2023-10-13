b5be5ede59458a883d4cfef127f8132945d760ed
FNMario <FedericoNMario@gmail.com>
2023-10-13 16:25:34 -0300

    feat: add ctrl+keys functionality in home screen

--- app/screens/home.py
+++ app/screens/home.py
@@ -11,29 +11,60 @@ import logging
 
 Builder.load_file('screens/home_screen.kv')
 
+
 class HomeWindow(Widget):
     maximized = False
-    
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.ids.home_screen_manager.transition = NoTransition()
+        Window.bind(on_key_down=self.on_key_down_home)
 
-    def change_category(self,instance):
+    def on_key_down_home(self, instance, keyboard, keycode, text, modifiers):
+        # print(self, instance, keyboard, keycode, text, modifiers, sep='\t')
+        if keycode == 41:   # Esc
+            if self.get_root_window():
+                self.log_out()
+                return True
+        if 'ctrl' in modifiers:
+            if keycode == 43:   # tab
+                self.ids.home_screen_manager.current = self.ids.home_screen_manager.next()
+            elif text == '1' or keycode == 89 and 'numlock' in modifiers:
+                self.ids.btn_buy.dispatch('on_press')
+                return True
+            elif text == '2' or keycode == 90 and 'numlock' in modifiers:
+                self.ids.btn_budgets.dispatch('on_press')
+                return True
+            elif text == '3' or keycode == 91 and 'numlock' in modifiers:
+                self.ids.btn_manage_prices.dispatch('on_press')
+                return True
+            elif text == '4' or keycode == 92 and 'numlock' in modifiers:
+                self.ids.btn_print_tables.dispatch('on_press')
+                return True
+            elif text == '5' or keycode == 93 and 'numlock' in modifiers:
+                self.ids.btn_settings.dispatch('on_press')
+                return True
+
+    # Categories
+
+    def change_category(self, instance):
         if instance.active:
             instance.active = False
             self.ids.home_screen_manager.current = "none"
-        else:           
+        else:
             for child in self.ids.categories_layout.children:
                 child.active = False
             instance.active = True
             self.ids.home_screen_manager.current = instance.screen_name
-    
+
     def log_out(self):
+        Window.unbind(on_key_down=self.on_key_down_home)
         for child in self.ids.categories_layout.children:
             child.active = False
         self.ids.home_screen_manager.current = "none"
         self.parent.parent.parent.ids.main_screen_manager.current = "login"
-    
+        self.parent.parent.parent.ids.login_screen.children[0].ids.txt_username.focus = True
+
     def minimize(self):
         Window.minimize()
 
@@ -43,6 +74,7 @@ class HomeWindow(Widget):
         else:
             Window.maximize()
         self.maximized = not self.maximized
+
 
 class HomeApp(App):
     def build(self):
