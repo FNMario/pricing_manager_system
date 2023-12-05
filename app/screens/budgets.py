664a968ac8b2e3f0218e3380913ad9007d88621f
FNMario <FedericoNMario@gmail.com>
2023-12-05 12:19:52 -0300

    feat: create structure of budgets screen

--- app/screens/budgets.py
+++ app/screens/budgets.py
@@ -8,4 +8,39 @@ Builder.load_file('screens/budgets_screen.kv')
 
 
 class Budgets(Screen):
-    pass
+
+    def search_budget(self, return_items: bool = False):
+        if return_items:
+            return []
+
+    def clear_form(self, form):
+        pass
+
+    def on_tbl_budgets_list_selected_row(self):
+        pass
+
+    def btn_save_header(self):
+        pass
+
+    def on_tbl_budget_selected_row(self):
+        pass
+
+    def btn_save_item(self):
+        pass
+
+    def btn_save_item(self):
+        pass
+
+    def btn_edit_budget_on_press(sef):
+        pass
+
+    def btn_refresh_budget_on_press(sef):
+        pass
+
+    def btn_discard_budget_on_press(sef):
+        pass
+
+    def all_upper_case(self, instance, max_length: int = None):
+        instance.text = instance.text.upper()
+        if type(max_length) == int:
+            instance.text = instance.text[:max_length]
