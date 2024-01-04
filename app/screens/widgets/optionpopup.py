6bf9b2eb9d8088032d12027b26c787035155add0
FNMario <FedericoNMario@gmail.com>
2024-01-04 16:02:50 -0300

    feat: select option pressing numeric keys

--- app/screens/widgets/optionpopup.py
+++ app/screens/widgets/optionpopup.py
@@ -1,6 +1,7 @@
 from kivy.properties import StringProperty, ListProperty, NumericProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
+from kivy.core.window import Window
 from kivy.lang.builder import Builder
 
 Builder.load_file('screens/widgets/optionpopup.kv')
@@ -29,6 +30,7 @@ class OptionPopup(Popup):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         options_list = self.ids.bl_list
+        Window.bind(on_key_down=self.on_key_down_self)
 
         for item, option in enumerate(self.options):
             op = OptionItem(item=item, text=option)
@@ -39,3 +41,23 @@ class OptionPopup(Popup):
     def option_pressed(self, option):
         self.selected_option = option.item
         self.dismiss()
+
+    def on_key_down_self(self, instance, keyboard, keycode, text, modifiers):
+
+        if keyboard > 48 and keyboard < 58:
+            number_key = keyboard - 49
+        elif keyboard > 256 and keyboard < 266 and 'numlock' in modifiers:
+            number_key = keyboard - 257
+        else:
+            number_key = None
+
+        if keyboard == 41:   # ESC
+            self.dismiss()
+        elif number_key in range(len(self.options)):
+            self.selected_option = number_key
+            self.dismiss()
+
+        return True
+
+    def on_dismiss(self):
+        Window.unbind(on_key_down=self.on_key_down_self)
