734791308e4de0c5e24884277c0a541cd958fdd9
FNMario <FedericoNMario@gmail.com>
2024-02-01 16:17:03 -0300

    feat: add connection from login screen to interface

--- app/screens/login.py
+++ app/screens/login.py
@@ -2,6 +2,8 @@ from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 import logging
 
+from interface import login
+
 Builder.load_file('screens/login_screen.kv')
 
 
@@ -10,23 +12,34 @@ class LoginWindow(Screen):
     name = "login"
 
     def btn_login_on_press(self):
-        logging.debug('btn_login pressed')
         username = self.ids.txt_username
         password = self.ids.txt_password
         lbl_info = self.ids.lbl_info
 
-        if username.text == "" and password.text == "":
-            lbl_info.text = "[color=ff6666]Username and password can not be empty.[/color]"
+        if username.text == "":
+            lbl_info.text = "[color=ff6666]Neither the username nor password field can be empty.[/color]"
+            username.focus = True
+            return
 
-        elif username.text == "asd" and password.text == "asdf":
-            # lbl_info.text = "[color=66ff66]Logged in![/color]"
+        if password.text == "":
+            lbl_info.text = "[color=ff6666]Neither the username nor password field can be empty.[/color]"
+            password.focus = True
+            return
+
+        try:
+            logged = login(username.text, password.text)
+            if not logged:
+                raise ConnectionError
             lbl_info.text = ""
             username.text = ""
-            logging.info('Logged in')
+            logging.info(f"Logged in as {username.text}")
             self.parent.current = "home"
-
-        else:
+        except ConnectionError as e:
+            logging.error("Username and password do not match.")
             lbl_info.text = "[color=ff6666]Username and password do not match.[/color]"
+        except Exception as e:
+            logging.error(e)
+            lbl_info.text = f"[color=ff6666]{e}[/color]"
 
         password.text = ""
         username.focus = True
